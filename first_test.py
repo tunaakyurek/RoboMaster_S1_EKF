331018f5883bc7e698f9838dd264873b78948d67
Tuna <75180421+tunaakyurek@users.noreply.github.com>
2025-08-08 16:46:46 +0300

    Create first_test.py

---
+++ first_test.py
@@ -0,0 +1,9 @@
+from robomaster import robot
+
+ep = robot.Robot()
+ep.initialize(conn_type="ap", ip="192.168.2.1")
+
+print(ep.chassis.get_position())
+print(ep.chassis.get_attitude())
+
+ep.close()
